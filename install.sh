fd58a20b8c42c78e74f6a5f6b859c9bfad3c7dae
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-09-26 23:39:22 -0300

    Remove emacsOnRails, using prelude only now

--- install.sh
+++ install.sh
@@ -12,14 +12,16 @@ while [ $# -gt 0 ]; do
 done
 
 echo
-echo "--------------------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------"
 echo " WARNING: this will override:"
 echo "          ~/.gitconfig"
 echo "          ~/.gemrc"
 echo "          ~/.bash_aliases"
+echo "          ~/.bashrc"
+echo "          ~/.profile"
 echo "          ~/.emacs"
 echo "          ~/.emacs.d/"
-echo "--------------------------------------------------------------------------------"
+echo "-------------------------------------------------------------------------"
 echo
 
 if [ $FORCE_YES ]; then
@@ -28,13 +30,23 @@ else
     read -p "Proceed (y/N)?"
 fi
 
+PRELUDE_INSTALL_DIR="$HOME/.emacs.d"
+install_prelude () {
+  rm -rf $PRELUDE_INSTALL_DIR
+  curl -L https://github.com/bbatsov/prelude/raw/master/utils/installer.sh | sh
+}
+
 if [[ $REPLY == [yY] ]]; then
   git submodule init
   git submodule update
 
+  install_prelude
+
   ln -sfv $PWD/.gitconfig ~/.gitconfig
   ln -sfv $PWD/.gemrc ~/.gemrc
   ln -sfv $PWD/.bash_aliases ~/.bash_aliases
-  ln -sfv $PWD/emacsOnRails/.emacs ~/.emacs
-  rm -rf ~/.emacs.d && ln -sfv $PWD/emacsOnRails/.emacs.d ~/.emacs.d
+  ln -sfv $PWD/.bashrc ~/.bashrc
+  ln -sfv $PWD/.profile ~/.profile
+  #ln -sfv $PWD/.emacs ~/.emacs
+  rm -rf ~/.emacs.d/personal && ln -sfv $PWD/prelude-personal/personal ~/.emacs.d/personal
 fi
