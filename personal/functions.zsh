68a894c162e3c669e30a8e2a5b8b9900e5281238
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2021-10-04 15:03:00 -0300

    Add aliases ke and discord

--- personal/functions.zsh
+++ personal/functions.zsh
@@ -2,3 +2,8 @@ tw() {
     # livestreamer twitch.tv/$1 "${2:-best}" --ringbuffer-size 100M --hls-live-edge 12 -l ${3:-info}
     streamlink --twitch-disable-ads twitch.tv/$1 "${2:-best}"
 }
+
+discord () {
+    disc=$(find ~/Downloads -type f -name Discord -printf "\n%AD %AT %p" | sort -k1.8nr -k1.1nr -k1r | cut -d ' ' -s -f3 | head -1)
+    exec $disc > /dev/null 2>&1 &
+}
