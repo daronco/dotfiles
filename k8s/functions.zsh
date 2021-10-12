bbfc408040923b2d5aa50f18a93532e603cb2ffc
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2021-10-12 19:59:18 -0300

    Add function k-delete-evicted

--- k8s/functions.zsh
+++ k8s/functions.zsh
@@ -1,4 +1,4 @@
-update-do-kubeconfig () {
+do-update-kubeconfig () {
     readonly ctx=${1:?"The cluster must be specified."}
 
     TMP_CONFIG=~/.kube/bk/$ctx.yaml
@@ -20,5 +20,18 @@ update-do-kubeconfig () {
         print_success "All done!"
     else
         print_fail "Something wrong happened"
+    fi
+}
+
+k-delete-evicted () {
+    echo "Removing evicted pods:"
+    kubectl get pods | grep Evicted
+    echo "Proceed?"
+    if confirm
+    then
+        for each in $(kubectl get pods | grep Evicted | awk '{print $1}');
+        do
+            kubectl delete pods $each
+        done
     fi
 }
