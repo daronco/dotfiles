20b014ba633d778938fda05fcffff46043986bd7
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2021-10-11 21:37:21 -0300

    Update how streamlink is installed

--- system/install.sh
+++ system/install.sh
@@ -4,5 +4,5 @@ info "Installing system packages"
 APT_PACKAGES='wget curl guake zsh emacs aptitude lxc xclip tmux meld git'
 sudo apt-get install -y $APT_PACKAGES
 
-sudo pip install --upgrade pip
-sudo pip install --upgrade streamlink
+sudo pip install --upgrade setuptools pip
+sudo pip3 install --upgrade streamlink
