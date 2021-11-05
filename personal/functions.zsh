b599f7c7362807726339f17a49f06801e40b9d7b
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2021-11-04 22:58:13 -0300

    Add function tomp3

--- personal/functions.zsh
+++ personal/functions.zsh
@@ -7,3 +7,8 @@ discord () {
     disc=$(find ~/Downloads -type f -name Discord -printf "\n%AD %AT %p" | sort -k1.8nr -k1.1nr -k1r | cut -d ' ' -s -f3 | head -1)
     exec $disc > /dev/null 2>&1 &
 }
+
+# tomp3 input.m4a
+tomp3 () {
+    ffmpeg -i ${1} -c:v copy -c:a libmp3lame -q:a 4 output.mp3
+}
