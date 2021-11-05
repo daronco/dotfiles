3080936a75f322dea554d0cea628007dec8e5c4b
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2021-11-04 22:58:02 -0300

    Install youtube-dl

---
+++ personal/install.sh
@@ -0,0 +1,6 @@
+. $DOTFILES/_functions.sh
+
+info "Installing youtube-dl"
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /tmp/youtube-dl
+chmod a+rx /tmp/youtube-dl
+sudo mv /tmp/youtube-dl /usr/local/bin/youtube-dl
