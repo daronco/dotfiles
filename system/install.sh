3157ace649e6e97c72adee65c9d6ede145d303ad
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2022-01-17 10:03:25 -0300

    Fix streamlink if pip3 is not present

--- system/install.sh
+++ system/install.sh
@@ -5,4 +5,9 @@ APT_PACKAGES='wget curl guake zsh emacs aptitude lxc xclip tmux meld git'
 sudo apt-get install -y $APT_PACKAGES
 
 sudo pip install --upgrade setuptools pip
-sudo pip3 install --upgrade streamlink
+if ! command -v pip3 &> /dev/null
+then
+    sudo pip install --upgrade streamlink
+else
+    sudo pip3 install --upgrade streamlink
+fi
