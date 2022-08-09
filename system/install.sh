eb05e0490a6a28248f3505b3d08af73082b755ff
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2022-08-09 13:25:16 -0300

    Install gum

--- system/install.sh
+++ system/install.sh
@@ -1,9 +1,14 @@
 . $DOTFILES/_functions.sh
 
+# gum: https://github.com/charmbracelet/gum
+echo 'deb [trusted=yes] https://repo.charm.sh/apt/ /' | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install gum
+
 info "Installing system packages"
 APT_PACKAGES='wget curl guake zsh emacs aptitude lxc xclip tmux meld git'
 sudo apt-get install -y $APT_PACKAGES
 
+info "Installing python tools"
 sudo pip install --upgrade setuptools pip
 if ! command -v pip3 &> /dev/null
 then
