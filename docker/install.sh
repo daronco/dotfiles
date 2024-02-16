9f053da25a9eb3bb8a88c8d903822c5949af76ae
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2024-02-16 09:16:14 -0300

    Add docker/install.sh

---
+++ docker/install.sh
@@ -0,0 +1,19 @@
+. $DOTFILES/_functions.sh
+
+# Install docker from their repository, it's more up-to-date than Ubuntu's
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+DOCKER_VERSION=`sudo apt-cache madison docker-ce | awk '{ print $3 }' | head -1`
+info "Installing docker ${DOCKER_VERSION}"
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
